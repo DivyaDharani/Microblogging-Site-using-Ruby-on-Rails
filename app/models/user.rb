b6b1bcc82f20c7fc97cdacef05e6dd71280f9d4d
system <divya@CHR-045.(none)>
2016-05-25 17:05:20 +0530

    rails version 3.2.22.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@
 #  email      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
+#  
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name,:password,:password_confirmation
